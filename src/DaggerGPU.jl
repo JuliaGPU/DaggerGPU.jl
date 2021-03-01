bb16ce1d1c2638bbb35652c415f73844ee11937b
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2021-03-01 09:55:05 -0600

    Get ROCArrayProc working somewhat

--- src/DaggerGPU.jl
+++ src/DaggerGPU.jl
@@ -37,7 +37,7 @@ function __init__()
     @require CUDA="052768ef-5323-5732-b1bb-66c8b64840ba" begin
         include("cu.jl")
     end
-    @require ROCArrays="ddf941ca-5d6a-11e9-36cc-a3fed13dd2fc" begin
+    @require AMDGPU="21141c5a-9bdb-4563-92ae-f87d6854732e" begin
         include("roc.jl")
     end
 end
--- src/roc.jl
+++ src/roc.jl
@@ -1,19 +1,79 @@
 using .AMDGPU
 
+struct RemoteROCDevice
+    handle::UInt64
+    owner::Int
+end
+RemoteROCDevice(handle::UInt64) = RemoteROCDevice(handle, myid())
+# TODO: Instead take hash of identifying fields
+RemoteROCDevice(agent::AMDGPU.HSAAgent, pid=myid()) =
+    RemoteROCDevice(agent.agent.handle, pid)
+function agent(rrd::RemoteROCDevice)
+    rrd.owner == myid() || error("Cannot get agent of remote worker")
+    AMDGPU.HSAAgent(AMDGPU.HSA.Agent(rrd.handle))
+end
+
 struct ROCArrayProc <: Dagger.Processor
-    device
+    device::RemoteROCDevice
+    owner::Int
 end
 
 @gpuproc(ROCArrayProc, ROCArray)
+Dagger.get_parent(proc::ROCArrayProc) = Dagger.OSProc(proc.owner)
 
-Dagger.execute!(proc::ROCArrayProc, func, args...) = func(args...)
+function Dagger.execute!(proc::ROCArrayProc, func, args...)
+    tls = Dagger.get_tls()
+    task = Threads.@spawn begin
+        Dagger.set_tls!(tls)
+        set_default_agent(agent(proc.device)) do
+            func(args...)
+        end
+    end
+    try
+        fetch(task)
+    catch err
+        @static if VERSION >= v"1.1"
+            stk = Base.catch_stack(task)
+            err, frames = stk[1]
+            rethrow(CapturedException(err, frames))
+        else
+            rethrow(task.result)
+        end
+    end
+end
+function Dagger.move(from_proc::ROCArrayProc, to_proc::ROCArrayProc, x::Chunk)
+    if from_proc.owner == to_proc.owner
+        x_from = poolget(x.handle)
+        if from_proc.device.handle == to_proc.device.handle
+            return x_from
+        else
+            to_agent = agent(to_proc.device)
+            x_to = set_default_agent(to_agent) do
+                similar(x_from)
+            end
+            copyto!(x_to, x_from)
+            return x_to
+        end
+    else
+        x_cpu = remotecall_fetch(from_proc.owner) do
+            Dagger.move(from_proc, Dagger.OSProc(), x)
+        end
+        @assert !(x_cpu isa ROCArray)
+        return Dagger.move(OSProc(), to_proc, x_cpu)
+    end
+end
+Base.show(io::IO, proc::ROCArrayProc) =
+    print(io, "ROCArrayProc on worker $(proc.owner), device $(proc.owner == myid() ? agent(proc.device) : proc.device)")
+
 
 processor(::Val{:ROC}) = ROCArrayProc
 cancompute(::Val{:ROC}) = AMDGPU.configured
 # FIXME: kernel_backend(::ROCDevice) = ROCArrayProc
 
 if AMDGPU.configured
-    Dagger.add_callback!(proc -> begin
-        return ROCArrayProc(AMDGPU.get_default_agent())
-    end)
+    for agent in AMDGPU.get_agents(:gpu)
+        Dagger.add_callback!(proc -> begin
+            return ROCArrayProc(RemoteROCDevice(agent), myid())
+        end)
+    end
 end
